3b8b23e69e2c311468c337c3ffdffd3ca4593e9b
pihrtmartin <pihrt.martin@gmail.com>
2026-08-14 06:40:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,38 @@
-self.addEventListener('fetch', function(event) {
-    // Service Worker pro PWA
+const CACHE_NAME = 'slovan-louny-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './Logo.png'
+];
+
+// Instalace Service Workeru a uložení souborů do cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Aktivace a vyčištění staré cache
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Zachytávání požadavků (Offline podpora)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
